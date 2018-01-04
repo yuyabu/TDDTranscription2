dd74340fa6c91a77980a1ae13c2a6b2a90493b33
yuyabu <yuyabushita@gmail.com>
2018-01-04 09:44:51 +0900

    実行済みフラグの行進を実装

--- xunit.py
+++ xunit.py
@@ -1,8 +1,11 @@
+class WasRun:
+    def __init__(self,name):
+        self.wasRun = None
+    def testMethod(self):
+        self.wasRun = 1
+
+
 test = WasRun("testMethod")
 print(test.wasRun)
 test.testMethod()
 print(test.wasRun)
-
-class WasRun:
-  def __init__(self,name):
-    pass
