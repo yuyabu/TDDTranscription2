b64a743f898242609756cf42224b02d240b7522b
yuyabu <yuyabushita@gmail.com>
2018-01-16 19:26:23 +0900

    20章まで進めた

--- xunit.py
+++ xunit.py
@@ -1,35 +1,31 @@
-u""" テストメソッドを動的に呼び出すクラス
-nameに与えた名前のメソッドをrunメソッドで呼び出す
-"""
 class TestCase:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
     def setUp(self):
         pass
     def run(self):
         self.setUp()
-        method = getattr(self,self.name)
+        method = getattr(self, self.name)
         method()
-u""" テストか呼び出されたかどうかを記録するクラス
 
-"""
 class WasRun(TestCase):
+
     def testMethod(self):
         self.wasRun = 1
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
-u""" テストケースを呼び出すクラス
-"""
+        self.log = "setUp"
+
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
