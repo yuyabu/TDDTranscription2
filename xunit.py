bf4aea1973358c5ef55a7966608b87d6e187785f
yuyabu <yuyabushita@gmail.com>
2018-01-17 20:54:02 +0900

    tearDownの実装。
    inconsistent use of tabs and spaces in indentation
    とかいうエラーがでまくってうざい
    エディタの改行時空白などの設定がうまくできていないんだと思う。

--- xunit.py
+++ xunit.py
@@ -1,23 +1,27 @@
 class TestCase:
-    def __init__(self, name):
-        self.name = name
-    def setUp(self):
-        pass
-    def run(self):
-        self.setUp()
-        method = getattr(self, self.name)
-        method()
-
+	def __init__(self, name):
+		self.name = name
+	def setUp(self):
+		pass
+	def run(self):
+		self.setUp()
+		method = getattr(self, self.name)
+		method()
+		self.tearDown()
+	def tearDown(self):
+		pass
 class WasRun(TestCase):
 	def testMethod(self):
 		self.log = self.log + "testMethod "
 	def setUp(self):
 		self.log = "setUp "
+	def tearDown(self):
+		self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
-    def testTemplateMethod(self):
+	def testTemplateMethod(self):
 		test = WasRun("testMethod")
-        test.run()
-        assert("setUp testMethod " == test.log)
+		test.run()
+		assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
