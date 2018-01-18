9a4d55e30f615ffe21af1d753b0d5682b2f79c71
yuyabu <yuyabushita@gmail.com>
2018-01-18 20:10:48 +0900

    run呼び出し時にrunCountの更新を実装

--- xunit.py
+++ xunit.py
@@ -4,16 +4,18 @@ class TestCase:
 	def setUp(self):
 		pass
 	def run(self):
+		result = TestResult()
+		result.testStarted()
 		self.setUp()
 		method = getattr(self, self.name)
 		method()
 		self.tearDown()
-		return TestResult()
+		return result
 	def tearDown(self):
 		pass
 class TestResult:
 	def __init__(self):
-		self.runCount = 1
+		self.runCount = 0
 	def testStarted(self):
 		self.runCount = self.runCount + 1
 	def summary(self):
