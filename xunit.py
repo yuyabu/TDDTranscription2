6dcaea68defc1f335e1760172becfeb87c159975
yuyabu <yuyabushita@gmail.com>
2018-01-23 18:47:11 +0900

    22章完了。

--- xunit.py
+++ xunit.py
@@ -7,8 +7,11 @@ class TestCase:
 		result = TestResult()
 		result.testStarted()
 		self.setUp()
-		method = getattr(self, self.name)
-		method()
+		try:
+			method = getattr(self, self.name)
+			method()
+		except:
+			result.testFailed()
 		self.tearDown()
 		return result
 	def tearDown(self):
@@ -16,10 +19,14 @@ class TestCase:
 class TestResult:
 	def __init__(self):
 		self.runCount = 0
+		self.errorCount = 0
 	def testStarted(self):
 		self.runCount = self.runCount + 1
+	def testFailed(self):
+		self.errorCount = self.errorCount + 1
+
 	def summary(self):
-		return "%d run, 0 failed" % self.runCount
+		return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class WasRun(TestCase):
 	def testMethod(self):
@@ -43,7 +50,14 @@ class TestCaseTest(TestCase):
 	def testFailedResult(self):
 		test = WasRun("testBrokenMethod")
 		result = test.run()
-		assert("1 run, 1failed" ==result.summary())
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-# TestCaseTest("testFailedResult").run()
+		assert("1 run, 1 failed" ==result.summary())
+	def testFailedResultFormatting(self):
+		result = TestResult()
+		result.testStarted()
+		result.testFailed()
+		assert("1 run, 1 failed" == result.summary())
+
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
+print(TestCaseTest("testFailedResultFormatting").run().summary())
